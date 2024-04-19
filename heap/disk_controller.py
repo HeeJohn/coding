04709eaf8d66ec529dd2a116d00d16081f2bcc02
HeeJohn <gjshj366@gmail.com>
2024-04-19 12:41:05 +0900

    041924

--- heap/disk_controller.py
+++ heap/disk_controller.py
@@ -3,30 +3,50 @@
 import heapq
 
 def process(end_at, tr, job) :
-    tr +=(end_at-job[0]+job[1]) # wait time + excution time
-    end_at +=job[1] # completion time
+    tr += (end_at - job[0] + job[1]) # 대기 시간 + 실행 시간
+    end_at += job[1] # 완료 시간
     return end_at, tr
 
 def sjf(jobs, end_at, heap) :
     while jobs and jobs[0][0] <= end_at: # 끝난 시간 이전에 요청된 작업 선택
         job = jobs.pop(0)
-        heapq.heappush(heap, (job[1], job)) #처리시간을 기준으로 힙 생성
+        heapq.heappush(heap, (job[1], job)) # 처리 시간을 기준으로 힙 생성
     return heap
 
 def solution(jobs):
-    tr= 0 # turning around time
-    end_at =0 # completion time
-    jobs.sorted(key=lambda x: x[0]) # 요청이 들어온 순서대로 정렬
-    heap=[]
-    
-    while heap or jobs : # 모든 jobs를 처리할 때까지 -> job이 heap 있을 수 있으므로 둘 다 체크
-        heap =sjf(jobs,end_at,heap)
+    tr = 0 # 턴어라운드 시간
+    end_at = 0 # 완료 시간
+    jobs.sort(key=lambda x: x[0]) # 요청이 들어온 순서대로 정렬
+    heap = []
+    total_tasks = len(jobs)
 
-        if heap : # 현재 종료시간을 기준으로 그 전에 들어온 요청이 있다는 것
-            sj = heapq.heappop(heap) #shortest job
-            end_at,tr = process(end_at,tr, sj)
-        else : #heap 비면, 먼저 요청이 들어온 job부터 처리
-            fr = jobs.pop(0) # first requested job
-            end_at,tr = process(end_at,tr, fr)
-        
-    return tr//len(jobs)
+    while heap or jobs : # 모든 작업를 처리할 때까지
+        heap = sjf(jobs, end_at, heap)
+
+        if heap : # 현재 종료 시간을 기준으로 그 전에 들어온 요청이 있다는 것
+            sj = heapq.heappop(heap) # 가장 짧은 작업
+            end_at, tr = process(end_at, tr, sj[1])
+        else : # heap이 비면, 먼저 요청이 들어온 작업부터 처리
+            fr = jobs.pop(0) # 첫 번째 요청된 작업
+            end_at, tr = process(end_at, tr, fr)
+
+    return tr // total_tasks
+
+
+
+
+jobs = [[1, 4], [7, 9], [1000, 3]]
+# return = 5
+print(solution(jobs))
+
+jobs = [[7, 8], [3, 5], [9, 6]]
+# return = 9
+print(solution(jobs))
+
+jobs = [[0, 3], [10, 1]]
+# return 2
+print(solution(jobs))
+
+jobs = [[0, 3], [4, 4], [5, 3], [7, 1]]
+# return 4
+print(solution(jobs))
--- heap/double_priority_heap.py
+++ heap/double_priority_heap.py
@@ -1,32 +1,67 @@
-# def solution(operations):
-#     answer = []
+# Description
+# 이중 우선순위 큐는 다음 연산을 할 수 있는 자료구조를 말합니다.
+
+# 명령어	수신 탑(높이)
+# I 숫자	큐에 주어진 숫자를 삽입합니다.
+# D 1	큐에서 최댓값을 삭제합니다.
+# D -1	큐에서 최솟값을 삭제합니다.
+# 이중 우선순위 큐가 할 연산 operations가 매개변수로 주어질 때, 모든 연산을 처리한 후 큐가 비어있으면 [0,0] 비어있지 않으면 [최댓값, 최솟값]을 return 하도록 solution 함수를 구현해주세요.
+
+# 제한사항
+# operations는 길이가 1 이상 1,000,000 이하인 문자열 배열입니다.
+# operations의 원소는 큐가 수행할 연산을 나타냅니다.
+# 원소는 “명령어 데이터” 형식으로 주어집니다.- 최댓값/최솟값을 삭제하는 연산에서 최댓값/최솟값이 둘 이상인 경우, 하나만 삭제합니다.
+# 빈 큐에 데이터를 삭제하라는 연산이 주어질 경우, 해당 연산은 무시합니다.
+# 입출력 예
+# operations	return
+# ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]	[0,0]
+# ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]	[333, -45]
+# 입출력 예 설명
+# 입출력 예 #1
+
+# 16과 -5643을 삽입합니다.
+# 최솟값을 삭제합니다. -5643이 삭제되고 16이 남아있습니다.
+# 최댓값을 삭제합니다. 16이 삭제되고 이중 우선순위 큐는 비어있습니다.
+# 우선순위 큐가 비어있으므로 최댓값 삭제 연산이 무시됩니다.
+# 123을 삽입합니다.
+# 최솟값을 삭제합니다. 123이 삭제되고 이중 우선순위 큐는 비어있습니다.
+# 따라서 [0, 0]을 반환합니다.
+
+# 입출력 예 #2
+
+# -45와 653을 삽입후 최댓값(653)을 삭제합니다. -45가 남아있습니다.
+# -642, 45, 97을 삽입 후 최댓값(97), 최솟값(-642)을 삭제합니다. -45와 45가 남아있습니다.
+# 333을 삽입합니다.
+# 이중 우선순위 큐에 -45, 45, 333이 남아있으므로, [333, -45]를 반환합니다.
+
+
+
+import heapq
+
+def operate(operation, maxHeap,minHeap) :
+    cmd, num = operation.split()
     
+    if cmd == 'I':
+        num = int(num)
+        heapq.heappush(maxHeap, -num) 
+        heapq.heappush(minHeap, num) 
+    else :
+        if not maxHeap : return #무시 -> 동기화 되어있어서 가능
 
+        if num == '1':
+            minHeap.remove(-heapq.heappop(maxHeap)) #동기화
+        else:
+            if not minHeap: return #무시
+            maxHeap.remove(-heapq.heappop(minHeap)) #동기화
 
+def solution(operations):
+    maxHeap=[]
+    minHeap=[]
 
+    for operation in operations :
+        operate(operation,maxHeap,minHeap)
 
-
-# def insert_el_max(heap, el) :
-#     heap.append(el)
-#     parent  = len(heap)/2
-
-#     while parent > 0 :
-  
-#         left_child = heap[parent*2]
-#         right_child = heap[parent*2+1]
-
-#         bigger_child = parent*2 if left_child > right_child else parent*2+1 
-
-#         if heap[parent] < heap[bigger_child]:
-#             temp = heap[bigger_child]
-#             heap[bigger_child] = heap[parent]
-#             heap[parent] =temp
-        
-#         parent /=2
-
-#     return heap
-
-# def del_el_max(heap) :
-    
-
-#     heap.
+    if not maxHeap and not minHeap :
+        return [0,0]
+    else :
+        return[-heapq.heappop(maxHeap), heapq.heappop(minHeap)]
