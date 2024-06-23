0b486324f5d02cd4cab8409253be1d5ae5e5a36f
heejohn <gjshj366@gmail.com>
2024-06-23 23:40:53 +0900

    062324

--- baekjoon/javascript/1_basic/train.js
+++ baekjoon/javascript/1_basic/train.js
@@ -1,22 +1,25 @@
-// 입력을 받기 위한 readline 모듈을 가져옵니다.
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input : process.stdin,
+    output : process.stdout
 });
 
-let currentPassengers = 0;
-let maxPassengers = 0;
+let max = 0;
+let current = 0;
 
-// 한 줄씩 입력을 처리합니다.
-rl.on('line', (line) => {
-  const [out, in] = line.split(' ').map(Number);
-  currentPassengers = currentPassengers - out + in;
-  if (currentPassengers > maxPassengers) {
-    maxPassengers = currentPassengers;
-  }
-}).on('close', () => {
-  console.log(maxPassengers);
-  process.exit(0);
+rl.on("line", (line) => {
+    let [off, on] = line.split(" ").map(Number);
+    let diff = on - off;
+    solve(diff);
 });
+
+
+rl.on("close", ()=>{
+    console.log(max);
+});
+
+function solve(diff){
+    current += diff;
+    if(max < current) max = current;
+}
