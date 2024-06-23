ca0bc0ed863f16161be7e6d52109350ec4b93950
heejohn <gjshj366@gmail.com>
2024-06-24 01:46:37 +0900

    062424

--- baekjoon/javascript/1_basic/dwarf.js
+++ baekjoon/javascript/1_basic/dwarf.js
@@ -15,14 +15,67 @@ rl.on('line', (line)=>{
 });
 
 rl.on('close', ()=>{
-   
+   let result = solve(sum, dwarf);
+   result.sort((a, b) => a - b).forEach(function(number) {
+       console.log(number);
+   });
 });
 
 
 function solve(sum, dwarf){
     
     for(let i =0;i<dwarf.length;i++){
-        for(let j =0;j<dwarf.length-1;j++){
+        for(let j =0;j<dwarf.length;j++){
+            if(i===j) continue;
+            
+            if(sum - dwarf[i] - dwarf[j] === 100){
+                let result = [];
+                for (let index = 0 ; index < dwarf.length; index++){
+                    if(index === i || index === j) continue;
+                    result.push(dwarf[index]);
+                }
+                return result;
+            }
+        }
+    }
+    
+}
+
+
+
+///////////////////////////////////////////////////////
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+   input : process.stdin,
+   output : process.stdout
+});
+
+let dwarf = [];
+let sum = 0;
+
+rl.on('line', (line)=>{
+     let input = parseInt(line);
+     dwarf.push(input);
+     sum+=input;     
+});
+
+rl.on('close', ()=>{
+   let result = solve(sum, dwarf);
+   result.sort((a, b) => a - b).forEach(function(number) {
+       console.log(number);
+   });
+});
+
+
+function solve(sum, dwarf){
+    
+    for(let i =0;i<dwarf.length;i++){
+        for(let j =0;j<dwarf.length;j++){
+            if(i===j) continue;
+            
             if(sum - dwarf[i] - dwarf[j] === 100){
                 let result = [];
                 for (let index = 0 ; index < dwarf.length; index++){
